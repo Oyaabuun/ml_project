c6ef30d19c69d7729ecb86852dcf5ab2450655d2
harit dey <deyharit@gmail.com>
2024-05-20 21:47:36 +0530

    yo

--- housing/entity/config_entity.py
+++ housing/entity/config_entity.py
@@ -7,7 +7,7 @@ DataIngestionConfig=namedtuple("DataIngestionConfig",
 
 DataValidationConfig = namedtuple("DataValidationConfig", ["schema_file_path","report_file_path","report_page_file_path"])
 
-DataTransformationConfig = namedtuple("DataTransformationConfig", ["add_bedroom_per_room",
+DataTransformationConfig = namedtuple("DataTransformationConfig", ["add_bedroom_per_room", # this datatransformationconfig is geiven for featuring engineering
                                                                    "transformed_train_dir",
                                                                    "transformed_test_dir",
                                                                    "preprocessed_object_file_path"])
